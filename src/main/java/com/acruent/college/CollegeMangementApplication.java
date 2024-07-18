25ac0965dee97f880b4eda2164111040537939f4
saiganesh71362 <saiganesh71362@gmail.com>
2024-07-18 10:09:20 +0530

    first commit

---
+++ src/main/java/com/acruent/college/CollegeMangementApplication.java
@@ -0,0 +1,13 @@
+package com.acruent.college;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeMangementApplication.class, args);
+	}
+
+}
